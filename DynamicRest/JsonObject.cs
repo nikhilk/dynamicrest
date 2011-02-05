6d9eaea61c314d0112b5524f43592246afe35089
nikhilk <nikhilk@live.com>
2011-02-05 01:54:04 -0800

    Added License information.

--- DynamicRest/JsonObject.cs
+++ DynamicRest/JsonObject.cs
@@ -1,4 +1,10 @@
 // JsonObject.cs
+// DynamicRest provides REST service access using C# 4.0 dynamic programming.
+// The latest information and code for the project can be found at
+// https://github.com/NikhilK/dynamicrest
+//
+// This project is licensed under the BSD license. See the License.txt file for
+// more information.
 //
 
 using System;
