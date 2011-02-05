4408a7af3d5556eddc66ea0132e917c0837d0988
nikhilk <nikhilk@live.com>
2011-02-05 02:07:03 -0800

    Split out samples code into a Samples project separate from the main DynamicRest assembly.

---
+++ DynamicRest/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿// AssemblyInfo.cs
+// DynamicRest provides REST service access using C# 4.0 dynamic programming.
+// The latest information and code for the project can be found at
+// https://github.com/NikhilK/dynamicrest
+//
+// This project is licensed under the BSD license. See the License.txt file for
+// more information.
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DynamicRest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://www.nikhilk.net")]
+[assembly: AssemblyProduct("Dynamic REST")]
+[assembly: AssemblyCopyright("Copyright © 2011, Nikhil Kothari")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
